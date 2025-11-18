05f56e745700863d826acf8fe192dfe7e8353ab7
Trần Trung Hậu <44206038+trantrunghau22@users.noreply.github.com>
2025-11-18 11:18:49 +0700

    Comment out sys and os imports in main.py
    Comment out import statements and path adjustment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+# import sys
+# import os
+# sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from game.app import App
 
 def main():
