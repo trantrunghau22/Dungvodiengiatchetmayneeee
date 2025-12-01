ab9ddaa8864bbfade7bc9cd63f6dbd3d8870a9ac
Trần Trung Hậu <44206038+trantrunghau22@users.noreply.github.com>
2025-12-02 00:25:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import sys
 import os
 
-# Thêm đường dẫn hiện tại vào sys.path để Python nhận diện module 'game'
+
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from game.app import App
 
 def main():
     try:
-        # Khởi tạo ứng dụng
         app = App()
-        # Chạy vòng lặp game
         app.run()
     except Exception as e:
         print(f"Có lỗi xảy ra: {e}")
