b0126abdab5ec38b17560892c8bd718ad2528e47
Trần Trung Hậu <44206038+trantrunghau22@users.noreply.github.com>
2025-12-02 02:40:21 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import sys
 import os
 
-# Thêm đường dẫn hiện tại để Python tìm thấy thư mục 'game'
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from game.app import App
 
 def main():
-    # Khởi tạo và chạy game trực tiếp
     app = App()
     app.run()
 
